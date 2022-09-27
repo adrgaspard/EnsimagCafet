3d5f0e7e32940bcf24d34fadfa1e3a37174854ef
Gaspard <adr.gaspard@gmail.com>
2022-09-27 10:17:31 +0200

    Update ApplicationDbContextFactory.cs

--- EnsimagCafet.EntityFrameworkCore.DbMigrator/ApplicationDbContextFactory.cs
+++ EnsimagCafet.EntityFrameworkCore.DbMigrator/ApplicationDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace EnsimagCafet.EntityFrameworkCore.DbMigrator
     {
         private readonly Configuration _configuration;
 
-        public ApplicationDbContextFactory() : this(ConfigurationManager.OpenMappedExeConfiguration(new() { ExeConfigFilename = "App.config" }, ConfigurationUserLevel.None))
+        public ApplicationDbContextFactory() : this(ConfigurationManager.OpenMappedExeConfiguration(new() { ExeConfigFilename = "App.config" }, ConfigurationUserLevel.None)) { }
 
         public ApplicationDbContextFactory(Configuration configuration)
         {
