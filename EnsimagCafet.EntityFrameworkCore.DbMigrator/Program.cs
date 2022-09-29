fd378d9c275231955d2d7c074f69459a9a29d0ee
Gaspard <adr.gaspard@gmail.com>
2022-09-29 04:23:01 +0200

    Improve DbMigrator appsettings files

--- EnsimagCafet.EntityFrameworkCore.DbMigrator/Program.cs
+++ EnsimagCafet.EntityFrameworkCore.DbMigrator/Program.cs
@@ -1,9 +1,18 @@
 ﻿using EnsimagCafet.EntityFrameworkCore.DbMigrator;
 using Microsoft.Extensions.Configuration;
-using System.Configuration;
 
-var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).AddEnvironmentVariables().AddCommandLine(Array.Empty<string>()).Build();
+Console.WriteLine("Seeding database...");
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile("appsettings.Development.json", true, true)
+    .AddJsonFile("appsettings.Production.json", true, true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(Array.Empty<string>())
+    .Build();
 
 using var context = new ApplicationDbContextFactory(config).CreateDbContext(Array.Empty<string>());
 
 new ApplicationDbDataSeeder(new() { SuperUserDefaultPassword = config.GetSection("Seeding").GetSection("SuperUserDefaultPassword").Value }).Seed(context);
+
+Console.WriteLine("Database seeded !");
