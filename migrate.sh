6b8e1ac4b1872625f93b1cd4ba9d229b48bc0ba6
Gaspard <gaspard@sd-60822.dedibox.fr>
2022-09-29 04:49:43 +0200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-dotnet build EnsimagCafet.EntityFrameworkCore.DbMigrator/EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj -c Release \
-&& dotnet ef database update --project EnsimagCafet.EntityFrameworkCore.DbMigrator/EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj \
-&& dotnet run --project EnsimagCafet.EntityFrameworkCore.DbMigrator/EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj -c Release
+current_dir=$(pwd)
+
+cd EnsimagCafet.EntityFrameworkCore.DbMigrator \
+&& dotnet build EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj -c Release \
+&& dotnet ef database update --project EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj \
+&& dotnet run --project EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj -c Release
+cd $current_dir
