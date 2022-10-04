00ba99a7dabcd21cf84eceaba9346d6e105fec88
Gaspard <adr.gaspard@gmail.com>
2022-10-04 12:37:00 +0200

    Delete useless scripts

--- migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-current_dir=$(pwd)
-
-cd EnsimagCafet.EntityFrameworkCore.DbMigrator \
-&& dotnet build EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj -c Release \
-&& dotnet ef database update --project EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj \
-&& dotnet run --project EnsimagCafet.EntityFrameworkCore.DbMigrator.csproj -c Release
-cd $current_dir
